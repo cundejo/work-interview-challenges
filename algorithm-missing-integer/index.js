9b648ce3e34cea439801dc653007dacbe3629fed
Oliver Sosa <cundejo85@gmail.com>
2021-09-30 15:46:44 -0500

    included another algorithm problem

---
+++ algorithm-missing-integer/index.js
@@ -0,0 +1,25 @@
+function solution(A) {
+  const sortNumbers = (a, b) => {
+    return a - b;
+  };
+
+  const aSorted = A.sort(sortNumbers);
+
+  if (aSorted[0] !== 1) return 1;
+
+  if (aSorted[aSorted.length - 1] < 1) return 1;
+
+  let number = 1;
+  let i = 0;
+  while (i < 10) {
+    if (number < aSorted[i]) return number;
+
+    if (!aSorted[i]) return number++;
+
+    number = aSorted[i] + 1;
+    i++;
+  }
+}
+
+console.log(solution([1, 3, 6, 4, 1, 2]));
+// solution([1, 2, 3]);
