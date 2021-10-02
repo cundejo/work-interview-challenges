065202083f2154ec86451cd58fa8520db52a92a9
Oliver Sosa <cundejo85@gmail.com>
2021-10-01 23:39:43 -0500

    Search input

--- react-search-input/src/App.js
+++ react-search-input/src/App.js
@@ -1,7 +1,36 @@
+import {useState} from "react";
+
+const debounce = (func, wait) => {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      func.apply(context, args);
+    }, wait);
+  };
+};
+
+const searchBreweries = async (query) => {
+  if(query.length === 0) return [];
+  const response = await fetch(`https://api.openbrewerydb.org/breweries/search?query=${query}`);
+  return await response.json();
+}
+
 function App() {
+  const [result, setResult] = useState([]);
+
+  const handleChange = debounce(async (e) =>{
+    const breweries = await searchBreweries(e.target.value)
+    setResult(breweries.slice(0,5));
+  }, 300)
+
+
   return (
     <div>
-      Hello as das f
+      <input onChange={handleChange} />
+      {result.map((item)=> <p key={item.id}>{item.name}</p>)}
     </div>
   );
 }
