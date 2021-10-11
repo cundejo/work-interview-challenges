ff5b4a0d4bcd6b653f8031b4a8ecef5698cbd00c
Oliver Sosa <cundejo85@gmail.com>
2021-10-11 12:37:50 -0500

    Done react-sorting

--- react-sorting-articles/src/App.js
+++ react-sorting-articles/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -6,17 +6,43 @@ import Articles from './components/Articles';
 
 const title = "Sorting Articles";
 
+const orderBy = (by, arr) => {
+  if(by === 'upvotes'){
+    return arr.sort((a, b) => b.upvotes - a.upvotes);
+  }
+
+  if(by === 'date'){
+    return arr.sort((a, b) => new Date(b.date) - new Date(a.date));
+  }
+
+  return arr;
+}
+
 function App({articles}) {
+
+  const [sortBy, setSortBy] = useState('upvotes');
 
     return (
         <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
+            <h8k-navbar header={title} />
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button
+                  data-testid="most-upvoted-link"
+                  className="small"
+                  onClick={()=>setSortBy('upvotes')}
+                >
+                  Most Upvoted
+                </button>
+                <button
+                  data-testid="most-recent-link"
+                  className="small"
+                  onClick={()=>setSortBy('date')}
+                >
+                  Most Recent
+                </button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={orderBy(sortBy, articles)}/>
         </div>
     );
 
